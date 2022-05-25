f2ec06040c9088cb8099adc099a5d837a7f30677
Arian <arian_febrian_29rpl@student.smktelkom-mlg.sch.id>
2022-05-25 19:43:15 +0700

    Lesson-7

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -6,9 +6,10 @@ class Heart extends StatefulWidget {
 }
 
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
-  bool isFav = false;
+  bool Subscribe = false;
   late AnimationController _controller;
   late Animation<Color?> _colorAnimation;
+  late Animation<double> _sizeAnimation;
 
   @override
   void initState() {
@@ -19,23 +20,31 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
       vsync: this,
     );
 
-    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red).animate(_controller);
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red)
+        .animate(_controller);
 
-    _controller.addListener(() {
-      // print(_controller.value);
-      // print(_colorAnimation.value);
-    });
-
+    _sizeAnimation = TweenSequence(
+        <TweenSequenceItem<double>>[
+          TweenSequenceItem<double>(
+            tween: Tween<double>(begin: 30, end: 50),
+            weight: 50,
+          ),
+          TweenSequenceItem<double>(
+            tween: Tween<double>(begin: 50, end: 30),
+            weight: 50,
+          ),
+        ]
+    ).animate(_controller);
 
     _controller.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
         setState(() {
-          isFav = true;
+          Subscribe = true;
         });
       }
       if (status == AnimationStatus.dismissed) {
         setState(() {
-          isFav = false;
+          Subscribe = false;
         });
       }
     });
@@ -57,10 +66,10 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
             icon: Icon(
               Icons.favorite,
               color: _colorAnimation.value,
-              size: 30,
+              size: _sizeAnimation.value,
             ),
             onPressed: () {
-              isFav ? _controller.reverse() : _controller.forward();
+              Subscribe ? _controller.reverse() : _controller.forward();
             },
           );
         }
